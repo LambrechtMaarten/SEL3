23424445fab038c426bc4eb3fabfb48362fb9082
jaron <jaron.batens@gmail.com>
2026-03-29 21:52:45 +0200

    ongebruikte controller verwijderd

--- configs/subcontrollers/controller/registration.py
+++ configs/subcontrollers/controller/registration.py
@@ -1,18 +1,11 @@
 from configs.subconfiguration_map import SubConfigurationMap
 from configs.subcontrollers.controller.controller_configurations import ControllerConfiguration
-from src.controller.direction_controller import DirectionController
 from src.controller.one_direction_controller import OneDirectionController
-from src.controller.symmetry_controller import BasicSymmetryController
 
 
 def register():
     SubConfigurationMap.add_configuration(
         ControllerConfiguration,
         ControllerConfiguration("standard", OneDirectionController())
-    )
-
-    SubConfigurationMap.add_configuration(
-        ControllerConfiguration,
-        ControllerConfiguration("body_direction", DirectionController())
     )
 
